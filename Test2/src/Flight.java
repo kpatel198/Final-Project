3dfb895d911128eea97f6d22c0dbef264c15e210
MangoMoonCmd <63313133+MangoMoonCmd@users.noreply.github.com>
2020-05-03 20:37:33 -0400

    Flight Update

--- Test2/src/Flight.java
+++ Test2/src/Flight.java
@@ -9,18 +9,40 @@ public class Flight {
 public static void main(String[] args) throws Exception  {
 
 		createFlightTable();
+		postFlight();
 	}
 
 //Creates flight table in SQL database
 	public static void createFlightTable()throws Exception{
 		try{
 			Connection con = getConnection();
-			PreparedStatement create  = con.prepareStatement("CREATE TABLE IF NOT EXISTS flight(number int NOT NULL, fromCity varchar(255), toCity varchar(255), date varchar(255),time int(5), seats varchar(255),PRIMARY KEY(number))");
+			PreparedStatement create  = con.prepareStatement("CREATE TABLE IF NOT EXISTS flight(number int NOT NULL, fromCity varchar(255), toCity varchar(255), date date,time time, seats int,PRIMARY KEY(number))");
 			create.executeUpdate();
 
 		}catch(Exception e) {System.out.println(e);}
 		finally {System.out.println("Table created.");}
 	}
+
+	//Inserts statements into flight table **NEEDS TO COME FROM GUI
+public static void postFlight() throws Exception{
+	final String VAR1 = number.getText();
+	final String VAR2 = toCity.getText();
+	final String VAR3 = fromCity.getText();
+	final String VAR4 = date.getText();
+	final String VAR5 = time.getText();
+	final String VAR6 = seats.getText();
+
+	try {
+			Connection con = getConnection();
+			PreparedStatement posted = con.prepareStatement("INSERT INTO flight (number, fromCity, toCity, date, time, seats) VALUES ('"+ VAR1+ "','"+VAR2+"','"+VAR3+"','"+VAR4+"','"+VAR5+"','"+VAR6+"')");
+			posted.executeUpdate();
+		}catch (Exception e) {System.out.println(e);}
+		finally {
+			System.out.println("Insert Completed.");
+		}
+
+	}
+
 
 	//establishes connection to database
 	public static Connection getConnection () throws Exception {
