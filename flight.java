d9918bca1e250ff03731c1a5cce81ed4908b9a00
isabellahgomez <64042685+isabellahgomez@users.noreply.github.com>
2020-05-06 20:38:00 -0400

    Create flight.java

---
+++ flight.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Flight {
+
+	private int flightNum;
+	
+	
+	//flight number
+	public int getFlightNum() {
+		return flightNum;
+	}
+	
+	public void setFlightNum() {
+		this.flightNum = flightNum;
+	}
+	
+	
+}
