aae5f3a5e34c5f82b0703a0637fc2232079e3558
isabellahgomez <64042685+isabellahgomez@users.noreply.github.com>
2020-05-06 20:20:36 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,111 @@
+package application;
+
+public class Customer{
+	
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String address;
+	private String zip;
+	private String state;
+	private String ssn;
+	private String question;
+	private String answer;
+
+	public static void main(String[] args) throws Exception {
+		
+	}
+	
+	//first name
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName() {
+		this.firstName = firstName;
+	}
+	
+	//last name
+	public String getLast() {
+		return lastName;
+	}
+	
+	public void setLastName() {
+		this.lastName = lastName;
+	}
+
+	// username
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName() {
+		this.userName = userName;
+	}
+
+	//password
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword() {
+		this.password = password;
+	}
+	
+	// address
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress() {
+		this.address = address;
+	}
+	
+	//zip
+	public String getZip() {
+		return zip;
+	}
+	
+	public void setZip() {
+		this.zip = zip;
+	}
+	
+	//state
+	public String getState() {
+		return state;
+	}
+
+	public void setState() {
+		this.state = state;
+	}
+	
+	//ssn
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn() {
+	this.ssn = ssn;
+	}
+	
+	//question
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion() {
+	this.question = question;
+	}
+	
+	//answer
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer() {
+	this.answer = answer;
+	}
+	
+	
+	
